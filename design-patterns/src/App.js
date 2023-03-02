54437e1a7c032a143bf6dbec6283b90854f081fb
skyblasterstudios <duon0065@algonquinlive.com>
2023-03-02 01:34:29 -0500

    CurrentUserLoader initial commit

--- design-patterns/src/App.js
+++ design-patterns/src/App.js
@@ -1,11 +1,12 @@
 import { CurrentUserLoader } from "./CurrentUserLoader";
 import { UserInfo } from "./UserInfo";
+import { UserLoader } from "./UserLoader";
 
 function App() {
   return(
-    <CurrentUserLoader>
+    <UserLoader userId="234">
       <UserInfo />
-    </CurrentUserLoader>
+    </UserLoader>
   )
 }
 
