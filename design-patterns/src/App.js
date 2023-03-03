072305df5500afd0ef6e305f98e56f9f6031c899
skyblasterstudios <duon0065@algonquinlive.com>
2023-03-03 16:47:39 -0500

    Uncontrolled Onboarding Initial Commit

--- design-patterns/src/App.js
+++ design-patterns/src/App.js
@@ -1,21 +1,28 @@
-import { useState } from "react";
-import { ControlledModal } from "./ControlledModal";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
+
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={goToNext({ name: 'John Doe' })}>Next</button>
+  </>)
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={goToNext({ age: 100 })}>Next</button>
+  </>)
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step 3</h1>
+    <button onClick={goToNext({ hairColor: 'brown' })}>Next</button>
+  </>)
 
 function App() {
-  const [shouldShowModal, setShouldShowModal] = useState(false)
   return (
-    <>
-      <ControlledModal
-        shouldShow={shouldShowModal}
-        onRequestClose={() => { setShouldShowModal(false) }}
-      >
-        <h1>Hello!</h1>
-      </ControlledModal>
-      <button onClick={() => setShouldShowModal(!shouldShowModal)}>
-        {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
-      </button>
-    </>
-
+    <UncontrolledOnboardingFlow onFinish={data => console.log(data)}>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
   )
 }
 
