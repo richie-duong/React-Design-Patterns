edd20a237ba0ee99f63f566971b79ed7821804c6
skyblasterstudios <duon0065@algonquinlive.com>
2023-03-06 15:10:42 -0500

    Recursive Comp initial commit

--- design-patterns/src/App.js
+++ design-patterns/src/App.js
@@ -1,14 +1,31 @@
-import { ProductInfo } from "./ProductInfo";
-import { UserInfo } from "./UserInfo";
+import { RecursiveComponent } from "./RecursiveComponent";
+
+const nestedObject = {
+	a: 1,
+	b: {
+		b1: 4,
+		b2: {
+			b23: 'Hello',
+		},
+		b3: {
+			b31: {
+				message: 'Hi',
+			},
+			b32: {
+				message: 'Hi',
+			}
+		}
+	},
+	c: {
+		c1: 2,
+		c2: 3,
+	}
+}
 
 function App() {
-
-  return (
-    <>
-    <UserInfo userId="123" />
-    <ProductInfo productId="1234" />
-    </>
-  )
+	return (
+		<RecursiveComponent data={nestedObject}/>
+	);
 }
 
 export default App;
--- design-patterns/src/printProps.js
+++
@@ -1,7 +0,0 @@
-// Higher order components are never directly displayed in jsx, which is why HOC conventions doesn't require capitalized names
-export const printProps = Component => {
-    return (props) => {
-        console.log(props)
-        return <Component {...props} />
-    }
-}
